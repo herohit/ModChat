90dec6d8e4d5e49d04561380823cd34de8b6be9c
herohit <rohitnegi323@gmail.com>
2025-04-06 12:11:05 +0530

    basic setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
-import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
+import ChatRoom from "./pages/ChatRoom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
   return (
-    <>
-      <h1 className="">Hello</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<ChatRoom />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/register" element={<Register />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/ChatRoom.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+
+const ChatRoom = () => {
+  return <div>ChatRoom</div>;
+};
+
+export default ChatRoom;
