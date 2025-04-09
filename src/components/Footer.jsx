5a6f38a11c55dcf36ad6a1f237f40fb8403803f7
herohit <rohitnegi323@gmail.com>
2025-04-09 16:55:54 +0530

    footer for mobile

---
+++ src/components/Footer.jsx
@@ -0,0 +1,35 @@
+import { Button } from "@/components/ui/button";
+import { Home, Compass, User, Settings } from "lucide-react";
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+function FooterItem({ to, icon, label }) {
+  return (
+    <NavLink
+      to={to}
+      className={({ isActive }) =>
+        `flex flex-col items-center text-xs transition-colors ${
+          isActive ? 'text-primary' : 'text-muted-foreground hover:text-primary'
+        }`
+      }
+    >
+      {icon}
+      <span className="text-[10px] mt-1">{label}</span>
+    </NavLink>
+  )
+}
+
+const Footer = () => {
+  return (
+    <footer className="fixed bottom-0 left-0 right-0 z-50 dark:bg-[#0F1923] border-t md:hidden">
+      <div className="flex justify-between px-6 py-2">
+        <FooterItem icon={<Home className="w-5 h-5" strokeWidth={3} />} label="Home" to ={'/'} />
+        <FooterItem icon={<Compass className="w-5 h-5" strokeWidth={3} />} label="Explore" to={'/explore'}/>
+        <FooterItem icon={<User className="w-5 h-5" strokeWidth={3} />} label="Profile" to={'/profile'}/>
+        <FooterItem icon={<Settings className="w-5 h-5" strokeWidth={3} />} label="Settings" to={'/settings'}/>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
